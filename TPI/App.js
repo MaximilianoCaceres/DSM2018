574b00ee0fd26b0bc6e91699d1519290423819e1
Maximiliano Caceres <kpo_max@live.com>
2018-06-22 12:24:08 -0300

    Copiado lo del profe - TP base

--- TPI/App.js
+++ TPI/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {counter:0};
+  }
+  
+  changeCounter(val){
+    this.setState(prevState => (
+      {
+        counter:prevState.counter + val
+      }
+    ));
+  }
+
+  
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text>Carlos hijo de puta!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+            <Text>{this.state.counter}</Text>
+        
+        <Button title="+" onPress={()=>this.changeCounter(1)} />
+        <Button title="-" onPress={()=>this.changeCounter(-1)} />
+        
       </View>
     );
   }
@@ -19,5 +37,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
